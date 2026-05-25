89ede8a72c450e7d212528df7b00f94420dd0876
Jayshree Mohan <88263102+JayshreeMohan@users.noreply.github.com>
2026-05-25 05:57:09 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        //memorization 
+        int n = nums.length;
+        int []dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return result(dp,nums,n-1);
+    }
+
+    public int result(int[]dp, int[]nums, int n){
+        
+        if(n == 0){
+            return nums[0];
+        }
+
+        if(n < 0){
+            return 0;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        int pick = nums[n] + result(dp,nums,n-2);
+        int nonPick = 0 + result(dp,nums,n-1);
+
+        dp[n] = Math.max(pick,nonPick);
+        return dp[n];
+    }
+}
