74e58e8c01c6d4da3cd79839ac4471307ffb074f
Jayshree Mohan <88263102+JayshreeMohan@users.noreply.github.com>
2026-07-08 19:35:44 +0530

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+
+        int n = nums.length;
+        int []dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 2; i <= n; i++){
+            int pick = nums[i-1] + dp[i-2];
+            int nonPick = 0 + dp[i-1];
+
+            dp[i] = Math.max(nonPick,pick);
+        }
+        return dp[n];
+        
+    }
+}
