d116284858ac7f23371754c17438a45f9a21aeb5
Jayshree Mohan <88263102+JayshreeMohan@users.noreply.github.com>
2026-07-17 15:37:47 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character,Integer> hm1 = new HashMap<>();
+        for(char c : s.toCharArray()){
+            hm1.put(c,hm1.getOrDefault(c,0)+1);
+        }
+
+        HashMap<Character,Integer> hm2 = new HashMap<>();
+        for(char c : t.toCharArray()){
+            hm2.put(c,hm2.getOrDefault(c,0)+1);
+        }
+
+        return hm1.equals(hm2);
+
+    }
+}
